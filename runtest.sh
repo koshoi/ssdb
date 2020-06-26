9f57edd1930fd04ffaae200a05222d1ece82e031
Oleg Koshovets <o.koshovets@corp.mail.ru>
2020-06-27 01:53:29 +0300

    added testing mechanism + fixed first bug found by testing

---
+++ runtest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ssdb_ex="${1:-$PWD/ssdb}"
+
+global_test_dir=$(mktemp -d -t 'ssdb.test.XXXXXXXXXX')
+cp -r test "$global_test_dir"
+
+testlist=$(find "$global_test_dir/test" -maxdepth 1 -mindepth 1 -type d)
+testlist=($testlist)
+
+
+for test_name in "${testlist[@]}"; do
+	echo bash "$test_name" "$ssdb_ex"
+	bash "$test_name/test" "$ssdb_ex"
+done
