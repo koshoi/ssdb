258f9aa0ac6c41ade8ade94018c91cfa0643ef48
Oleg Koshovets <o.koshovets@corp.mail.ru>
2020-06-28 02:06:13 +0300

    metatags added + more bugs found by testing

--- runtest.sh
+++ runtest.sh
@@ -10,7 +10,6 @@ cp -r test "$global_test_dir"
 testlist=$(find "$global_test_dir/test" -maxdepth 1 -mindepth 1 -type d)
 testlist=($testlist)
 
-
 for test_name in "${testlist[@]}"; do
 	echo bash "$test_name" "$ssdb_ex"
 	bash "$test_name/test" "$ssdb_ex"
