b31d0149642e62a8a81891a1fcbedad4c617bd07
Oleg Koshovets <o.koshovets@corp.mail.ru>
2020-06-28 22:47:25 +0300

    rework tests + add depcheck as a command

--- runtest.sh
+++ runtest.sh
@@ -7,7 +7,7 @@ ssdb_ex="${1:-$PWD/ssdb}"
 global_test_dir=$(mktemp -d -t 'ssdb.test.XXXXXXXXXX')
 cp -r test "$global_test_dir"
 
-testlist=$(find "$global_test_dir/test" -maxdepth 1 -mindepth 1 -type d)
+testlist=$(find "$global_test_dir/test" -maxdepth 1 -mindepth 1 -type d | sort)
 testlist=($testlist)
 
 for test_name in "${testlist[@]}"; do
